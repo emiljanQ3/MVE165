5d3394e8b428e78f9baae810e9473c21717ec114
Emil <emiljanQ3@hotmail.com>
2020-03-31 12:12:34 +0200

    Computed parameters from constants

--- Assignment1/assignment1_dat.jl
+++ Assignment1/assignment1_dat.jl
@@ -12,12 +12,14 @@ petrol = 150000
 #Constants
 R = 1 #Petrol price in €/l
 M = 1.5 #Methanol price in €/l
+U_M = 0.2/0.9 #Unit methanol needed per unit biodiesel
+U_V = 1/0.9 #Unit vegetable oil needed per unit biodiesel
 S = [0.178, 0.216, 0.433] #Oil content in seeds for i in I [l/kg]
 Y = [2600, 1400, 900] #Yield for i in I [kg/he]
-B = [1.43, 1.29, 1.1] #Price for j in J [€/l]
+B = [1.43, 1.29, 1.16] #Price for j in J [€/l]
 T = [0.2, 0.05, 0] #Part tax of price for j In J
 #Parameters
 V = [0.05, 0.3, 1] #Part vegetable oil per diesel type
-O = [2600*0.178, 1400*0.216, 900*0.433] #Oil content l/he for different crops
-P = [13/75, 0.4255, 62/75] #Profit per litre of fuel for different fuel types
+O = Y .* S #Oil content l/he for different crops
+P = B .* (1 .- T) - V * M * U_M - (1 .- V) * R
 W = [5, 4.2, 1] #Water demand for each crop [Ml/ha]
