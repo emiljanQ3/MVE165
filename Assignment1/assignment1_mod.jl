3f1830ba29d7fbec51c66e0c014c63fc021bd36a
Emil <emiljanQ3@hotmail.com>
2020-04-20 12:50:15 +0200

    latest changes by emil

--- Assignment1/assignment1_mod.jl
+++ Assignment1/assignment1_mod.jl
@@ -1,8 +1,7 @@
 """
   Construct and returns the model of this assignment.
 """
-function buildBiofuelModel(data_file::String)
-  # The diet problem
+function buildBiofuelModel(data_file::String, waterFactor::Float64, areaFactor::Float64, petrolFactor::Float64)
   include(data_file)
   #I: set of  crops
   #J: set of fuels
@@ -12,7 +11,7 @@ function buildBiofuelModel(data_file::String)
   # P[j] amount of profit from selling j [€/l]
 
   #name the model
-  model = Model()
+  model = Model(with_optimizer(Gurobi.Optimizer, OutputFlag=0))
 
   @variable(model, x[I] >= 0) # crops grown [ha]
   @variable(model, y[J] >= 0) # fuel produced [l]
@@ -20,14 +19,15 @@ function buildBiofuelModel(data_file::String)
   #maximize the profit
   @objective(model, Max, sum(P[j]*y[j] for j in J))
 
+  modifiedWater = water*waterFactor
+  modifiedArea = area*areaFactor
+  modifiedPetrol = petrol*petrolFactor
 
-
-
-  @constraint(model, areaConstraint, sum(x[i] for i in I) <= area) #
+  @constraint(model, areaConstraint, sum(x[i] for i in I) <= modifiedArea) #
   @constraint(model, fuelDemand, sum(y[j] for j in J) >= minFuel)
-  @constraint(model, waterConstraint, sum(x[i]*W[i] for i in I) <= water)
-  @constraint(model, petrolConstraint, sum(y[j]*(1-V[j]) for j in J) <= petrol)
-  @constraint(model, vegOilConstraint, sum(y[j]*(V)[j] for j in J) <= sum(x[i]*O[i] for i in I))
+  @constraint(model, waterConstraint, sum(x[i]*W[i] for i in I) <= modifiedWater)
+  @constraint(model, petrolConstraint, sum(y[j]*(1-V[j]) for j in J) <= modifiedPetrol)
+  @constraint(model, vegOilConstraint, sum(y[j]*(V)[j]*U_V for j in J) <= sum(x[i]*O[i] for i in I))
 
   return model, x, y
 end
--- Assignment1/assignment1_run.jl
+++ Assignment1/assignment1_run.jl
@@ -1,12 +1,25 @@
 using JuMP      #load the package JuMP
 using Gurobi   #The commercial optimizer Gurobi requires installation
-
+using Plots
 
 include("assignment1_mod.jl")
-model, x, y = buildBiofuelModel("assignment1_dat.jl")
 
-set_optimizer(model, Gurobi.Optimizer)
-optimize!(model)
+plotX = []
+plotY = []
 
-println("x = ",value.(x.data))
-println("y = ",value.(x.data))
+for factor in 0:0.01:2
+    model, x, y = buildBiofuelModel("assignment1_dat.jl", factor, factor, 1.0)
+
+    optimize!(model)
+    append!(plotX, factor)
+    append!(plotY, objective_value(model))
+
+    println(primal_status(model))
+    println("objective value = ", objective_value(model))
+    println("x = ",value.(x.data))
+    println("y = ",value.(y.data))
+    println(factor)
+    println(objective_value(model)/604843.8476190475)
+end
+
+plot(plotX, plotY)
